dfe90fbdacee3a1ad7f887fcf97059c98664e35c
Peter Sparks <petersparks18@gmail.com>
2019-01-28 20:51:40 +0000

    few minor fixes to get the site to work better

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get 'portfolio', to: 'pages#portfolio', as: :portfolio
+  get 'contact', to: 'pages#contact', as: :contact
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
