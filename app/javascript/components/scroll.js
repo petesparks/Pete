57503ba50db7f94136025e3f1bfe0179e4e5cb9a
Peter Sparks <petersparks18@gmail.com>
2019-01-28 19:50:30 +0000

    removed js scoll to for now

--- app/javascript/components/scroll.js
+++ app/javascript/components/scroll.js
@@ -1,7 +1,7 @@
 
-function scrollFunction() {
-  var navbarItem = document.querySelector("#contact");
-  navbarItem.scrollIntoView(true, {behaviour: "smooth"});
-}
+// function scrollFunction() {
+//   var navbarItem = document.querySelector("#contact");
+//   navbarItem.scrollIntoView(true, {behaviour: "smooth"});
+// }
 
-export { scrollFunction };
+// export { scrollFunction };
